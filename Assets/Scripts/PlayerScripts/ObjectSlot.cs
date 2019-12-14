75765a849d1d11bf359bca14bc60740fec3e65ef
Forky <boohoochicken@gmail.com>
2019-12-14 18:08:47 +0200

    Added player climb mechanic

--- Assets/Scripts/PlayerScripts/ObjectSlot.cs
+++ Assets/Scripts/PlayerScripts/ObjectSlot.cs
@@ -65,62 +65,6 @@ public class ObjectSlot : MonoBehaviour
         {
             moveScript.enabled = false;
         }
-
-        //Animal handling
-        //if (moveScript.character == PlayerMove.PlayerCharacters.Animal)
-        //{
-            
-        //    //Only check first object for the animal cause he can only carry one
-        //    if (heldObjects[0].tag == "Leg")
-        //    {
-               
-        //        moveScript.enabled = true;
-        //    }
-
-        //    else
-        //    {
-                
-        //        moveScript.enabled = false;
-        //    }
-
-        //}
-
-        ////Robot handling 
-        //if(moveScript.character == PlayerMove.PlayerCharacters.Robot)
-        //{
-        //    NrLegs = 0;
-        //    NrHands = 0;
-
-        //    foreach (Transform go in heldObjects)
-        //    {
-                
-        //        if (go.tag == "Leg")
-        //        {
-      
-        //            NrLegs += 1;
-                    
-        //        }
-
-        //        if(go.tag == "Hand")
-        //        {
-
-        //            NrHands += 1;
-        //        }
-                
-        //    }
-            
-        //    if ( NrLegs > 0)
-        //    {
-        //        moveScript.enabled = true;
-        //    }
-        //    else
-        //    {
-        //        moveScript.enabled = false;
-        //    }
-
-        //    //Here comes the hand
-
-        //}
         
     }
 }
