62f9e8858864a71b860632ef14cde32ca8055b4c
Forky <boohoochicken@gmail.com>
2019-12-15 17:45:28 +0200

    Added Text Boxes

---
+++ Assets/Scripts/TextBoxSystem/DialogueManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI textbox;
+
+    private Queue<string> dialogue;
+
+    private void Start()
+    {
+        dialogue = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue text)
+    {
+        dialogue.Clear();
+
+        foreach(string sentence in text.dialogue)
+        {
+            dialogue.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+
+        if(dialogue.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = dialogue.Dequeue();
+        textbox.text = sentence;
+    }
+
+    public void EndDialogue()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/TextBoxSystem/DialogueTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+
+    public Dialogue dialogue;
+
+    // Start is called before the first frame update
+    public void TriggerDialogue()
+    {
+        
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Robot"))
+        {
+            
+            TriggerDialogue();
+        }
+
+
+    }
+}
